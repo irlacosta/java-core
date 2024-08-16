0b0a8719b73b0b29fb94f962cdbd04a4c2914407
irLa Costa <irlasilva808@gmail.com>
2024-08-16 20:21:03 -0300

    Implemented calculator operations: add and subtract.

--- src/MinhaPrimeiraClasse.java
+++ src/MinhaPrimeiraClasse.java
@@ -2,15 +2,12 @@ import com.exemplo.playground.MinhaCalculadora;
 
 public class MinhaPrimeiraClasse {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-
-        var objetoCalculadora = new MinhaCalculadora();
-        var soma = objetoCalculadora.somar(4,5);
-        var subtracao = MinhaCalculadora.subtrair(4,5);
-
-        System.out.println("O somatório é: " + soma);
-        //System.out.println("A subtração é: " + subtracao(4,5));
-        System.out.println("A subtração é: " + MinhaCalculadora.subtrair(4,5));
+        System.out.println("Bem-vindo a Calculadora");
+        System.out.println("Escolha a operação:");
+        System.out.println("1. Somar");
+        System.out.println("2. Subtrair");
+        System.out.println("Informe um número: ");
+        System.out.println("Informe outro número: ");
     }
 
 }
