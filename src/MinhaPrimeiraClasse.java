5b3e32015313ee0efa99b0b693f5a7017dc99b90
irLa <irlasilva808@gmail.com>
2024-08-15 21:13:10 -0300

    Meu primeiro commit Aula 01

--- src/MinhaPrimeiraClasse.java
+++ src/MinhaPrimeiraClasse.java
@@ -1,5 +1,20 @@
 public class MinhaPrimeiraClasse {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        var somatorio = soma(2,4);
+        var subtracao = subtracao(6,1);
+
+        System.out.println("O somatório é: " + somatorio);
+        System.out.println("A subtração é: " + subtracao);
+        System.out.println("Hello World");
+
+    }
+
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtracao(int a, int b) {
+        return a - b;
     }
 }
