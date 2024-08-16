718df21d2449fa18d5d68c1054878f56e4d7e9f9
irLa Costa <irlasilva808@gmail.com>
2024-08-16 19:05:25 -0300

    Created a new class to demonstrate method calls with and without static in Java lesson 01

--- src/MinhaPrimeiraClasse.java
+++ src/MinhaPrimeiraClasse.java
@@ -1,20 +1,16 @@
+import com.exemplo.playground.MinhaCalculadora;
+
 public class MinhaPrimeiraClasse {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        var somatorio = soma(2,4);
-        var subtracao = subtracao(6,1);
 
-        System.out.println("O somatório é: " + somatorio);
-        System.out.println("A subtração é: " + subtracao);
-        System.out.println("Hello World");
+        var objetoCalculadora = new MinhaCalculadora();
+        var soma = objetoCalculadora.somar(4,5);
+        var subtracao = MinhaCalculadora.subtrair(4,5);
 
+        System.out.println("O somatório é: " + soma);
+        //System.out.println("A subtração é: " + subtracao(4,5));
+        System.out.println("A subtração é: " + MinhaCalculadora.subtrair(4,5));
     }
 
-    public static int soma(int a, int b) {
-        return a + b;
-    }
-
-    public static int subtracao(int a, int b) {
-        return a - b;
-    }
 }
---
+++ src/com/exemplo/playground/MinhaCalculadora.java
@@ -0,0 +1,13 @@
+package com.exemplo.playground;
+
+public class MinhaCalculadora {
+
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtrair(int a, int b) {
+        return a - b;
+    }
+
+}
