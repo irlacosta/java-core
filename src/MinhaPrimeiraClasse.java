c1b284481ae815bc15f690c19ea6afb0b5163839
irLa Costa <irlasilva808@gmail.com>
2024-08-16 20:51:25 -0300

    Implemented calculator operations with if-else logic for handling different cases.

--- src/MinhaPrimeiraClasse.java
+++ src/MinhaPrimeiraClasse.java
@@ -1,13 +1,32 @@
 import com.exemplo.playground.MinhaCalculadora;
 
+import java.util.Scanner;
+
 public class MinhaPrimeiraClasse {
     public static void main(String[] args) {
         System.out.println("Bem-vindo a Calculadora");
         System.out.println("Escolha a operação:");
         System.out.println("1. Somar");
         System.out.println("2. Subtrair");
-        System.out.println("Informe um número: ");
-        System.out.println("Informe outro número: ");
+        Scanner scanner = new Scanner(System.in);
+        Integer operacao = scanner.nextInt();
+
+        if (operacao == 1) {
+            System.out.println("Operação SOMA escolhida");
+        }else if (operacao == 2) {
+            System.out.println("Operação SUBTRAÇÃO escolhida");
+        }else {
+            System.out.println("Operação não existe");
+        }
+
+
+//        System.out.println("Informe um número: ");
+//        Scanner scannerFirstNumber = new Scanner(System.in);
+//        scannerFirstNumber.nextLine();
+//
+//        System.out.println("Informe outro número: ");
+//        Scanner scannerSecondNumber = new Scanner(System.in);
+//        scannerSecondNumber.nextLine();
     }
 
 }
